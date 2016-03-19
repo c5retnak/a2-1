0d2b172f7e9366326fc30dd69ea704854bc9834b
c5retnak <fast-puppy@hotmail.com>
2016-03-19 14:27:20 -0400

    Update grid_peg_solitaire_puzzle.py
    str and eq and is_solved good, have potential algorithm for extensions.

--- grid_peg_solitaire_puzzle.py
+++ grid_peg_solitaire_puzzle.py
@@ -24,18 +24,107 @@ class GridPegSolitairePuzzle(Puzzle):
         assert all([x == "*" or x == "." or x == "#" for x in marker_set])
         self._marker, self._marker_set = marker, marker_set
 
-    # TODO
-    # implement __eq__, __str__ methods
-    # __repr__ is up to you
+       def __eq__(self, other):
+        """
+        Return whether GridPegSolitairePuzzle self is equivalent to other.
 
-    # TODO
-    # override extensions
-    # legal extensions consist of all configurations that can be reached by
-    # making a single jump from this configuration
+        @type self: GridPegSolitairePuzzle
+        @type other: GridPegSolitairePuzzle | Any
+        @rtype: bool
 
-    # TODO
-    # override is_solved
-    # A configuration is solved when there is exactly one "*" left
+        >>> grid1 = []
+        >>> grid1.append(["*", "*", ".", "*", "*"])
+        >>> grid1.append(["*", "*", "*", "*", "*"])
+        >>> gpsp1 = GridPegSolitairePuzzle(grid1, {"*", ".", "#"})
+        >>> grid2 = []
+        >>> grid2.append(["*", "*", ".", "*", "*"])
+        >>> grid2.append(["*", "*", "*", "*", "*"])
+        >>> gpsp2 = GridPegSolitairePuzzle(grid2, {"*", ".", "#"})
+        >>> gpsp1 == gpsp2
+        True
+        >>> grid3 = []
+        >>> grid3.append(["*", "*", "*", "*", "*"])
+        >>> grid3.append(["*", "*", ".", "*", "*"])
+        >>> gpsp3 = GridPegSolitairePuzzle(grid3, {"*", ".", "#"})
+        >>> gpsp1 == gpsp3
+        False
+        """
+
+        return (type(self) == type(other)
+                and (self._marker == other._marker)
+                and (self._marker_set == other._marker_set))
+
+    def __str__(self):
+        """
+        Return a human-readable string representation of GridPegSolitairePuzzle self.
+
+        >>> grid = []
+        >>> grid.append(["*", "*", ".", "*", "*"])
+        >>> grid.append(["*", "*", "*", "*", "*"])
+        >>> gpsp = GridPegSolitairePuzzle(grid, {"*", ".", "#"})
+        >>> print(gpsp)
+        **.**
+        *****
+        """
+
+        # rows = [[row[i] for i in range(len(row))]
+        #         for row in self._marker]
+        # return "\n".join(rows)
+
+        gpsp = ""
+        j = 0
+        for row in self._marker:
+            j += 1
+            for i in range(len(row)):
+                gpsp += row[i]
+            if j != len(self._marker):
+                gpsp += "\n"
+        return gpsp
+
+    def __repr__(self):
+        pass
+
+    def is_solved(self):
+        """
+        Return True iff GridPegSolitairePuzzle self is solved.
+
+        @type self: GridPegSolitairePuzzle
+        @rtype: bool
+
+        >>> grid = []
+        >>> grid.append([".", ".", ".", ".", "."])
+        >>> grid.append([".", ".", "*", ".", "."])
+        >>> gpsp = GridPegSolitairePuzzle(grid, {"*", ".", "#"})
+        >>> gpsp.is_solved()
+        True
+        """
+
+        return [self._marker[i].count("*") == 1 for i in range(len(self._marker))].count(True) == 1
+
+    def extensions(self):
+        """
+        Return list of legal extensions of GridPegSolitairePuzzle self.
+
+        @type self: GridPegSolitairePuzzle
+        @rtype: list[GridPegSolitairePuzzle]
+        """
+
+        # convenient name
+        all_spots = self._marker
+
+        lex = []
+
+        return lex
+
+    # create empty_space_list
+        # [spot[r][c], ...]
+    # for every spot in empty_space_list
+        # above: if spot[r-2][c] exists and not # and spot[r-1][c] not in empty_space_list
+        # below: if spot[r+2][c] exists and not # and spot[r+1][c] not .
+        # left: if spot[r][c-2] exists and not # "
+        # right: if spot[r][c+2] exists and not # "
+            # create new grid, then add it
+            # lex.append(GridPegSolitaire())
 
 
 if __name__ == "__main__":
