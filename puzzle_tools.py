42fb8ff772357513753d00939d15074e6cdad12a
Rafi <rafira@me.com>
2016-03-29 16:46:52 -0400

    Implemented DFS.
    All puzzles except grid peg solitaire solve under 30 seconds. Grid peg solitaire seems to not solve properly. Will check that out and work on BFS next.

--- puzzle_tools.py
+++ puzzle_tools.py
@@ -11,12 +11,6 @@ from collections import deque
 import sys
 sys.setrecursionlimit(10**6)
 
-
-# TODO
-# implement depth_first_solve
-# do NOT change the type contract
-# you are welcome to create any helper functions
-# you like
 def depth_first_solve(puzzle):
     """
     Return a path from PuzzleNode(puzzle) to a PuzzleNode containing
@@ -26,35 +20,30 @@ def depth_first_solve(puzzle):
     @type puzzle: Puzzle
     @rtype: PuzzleNode
     """
+    return depth_first_search(puzzle, set())
+
+def depth_first_search(puzzle, visited):
+    """
+    Perform a depth first search of this puzzle and its respective extensions,
+    returning a PuzzleNode of this puzzle containing a path to a solution. If
+    no solution exists, return None.
+
+    @type puzzle: Puzzle
+    @type visited: set
+    @rtype: PuzzleNode | None
+    """
+    extensions = puzzle.extensions()
     if puzzle.is_solved():
         return PuzzleNode(puzzle)
-    elif puzzle.fail_fast() or not puzzle.extensions():
+    elif not extensions or puzzle.fail_fast():
         return None
-
-
-
-   # visited = set()
-   # extensions = puzzle.extensions()[:]
-   # while not puzzle.is_solved():
-   #     print(puzzle)
-   #     while extensions:
-   #         new_config = extensions.pop()
-   #         if str(new_config) not in visited:
-   #             visited.add(str(new_config))
-   #             extensions.extend(new_config.extensions())
-
-    #print(puzzle)
-    #unvisited = set(str(puzzle.extensions()))
-    #if puzzle.is_solved():
-    #    return PuzzleNode(puzzle)
-    #elif not puzzle.extensions():
-    #    return None
-    #else:
-    #    for ext in puzzle.extensions():
-    #        if str(ext) in unvisited:
-    #            unvisited -= set(ext)
-    #            return PuzzleNode(puzzle, [depth_first_solve(ext)])
-
+    else:
+        visited.add(str(puzzle))
+        for extension in extensions:
+            if not str(extension) in visited:
+                search = depth_first_search(extension, visited)
+                if search is not None:
+                    return PuzzleNode(puzzle, [search])
 
 # TODO
 # implement breadth_first_solve
@@ -73,74 +62,24 @@ def breadth_first_solve(puzzle):
     @rtype: PuzzleNode
     """
 
+    pn = PuzzleNode(puzzle, [])
     # Adds all the extensions in level-order and excludes repeated cases.
-    possible_sols = unique_extensions(puzzle)
-    while not possible_sols.is_empty():
+    possible_sols = pn.extensions()
+    while not level_nodes.is_empty():
         # Checks the possible extensions
-        node = possible_sols.popleft()
-        # If it is a solution, it returns the PuzzleNode so that PuzzleNode.children contains the solution.
-        if node.is_solved():
-            puzzle.children.append(node)
-            return puzzle
-        # If it is not, it adds the PuzzleNode it just checked anyways, so that if the solution is found in the future
-        # it remembers the path to it
-        else:
-            puzzle.children.append(node)
-    # If the possible_sols is empty, it means no solution was returned and found.
+        node = possible_sols.pop(0)
+        if unseen(node, seen_list=set()):
+            # If it is a solution, it returns the PuzzleNode so that
+            # PuzzleNode.children contains the solution.
+            if node.is_solved():
+                pn.children.append(node)
+                return pn
+            else:
+                return pn.children.append(breadth_first_solve(node))
+    # If the possible_sols is empty, it means no solution was returned and
+    # found.
     return None
 
-def unique_extensions(puzzle):
-    """
-    @param puzzle: Puzzle
-    @rtype: deque
-    """
-
-    #1
-    # Base case: if there are no more extensions, add the PuzzleNode if it's not already in the Queue.
-    uniques_extns = deque()
-    uniques_extns.append(puzzle)
-
-    if not puzzle.extensions():
-        if puzzle not in uniques_extns:
-            uniques_extns.append(puzzle)
-    # Recursive case: if there are extensions, run the function on each PuzzleNode in the set of extensions.
-    # J: problem, is it going to do it by level order or through depth?
-    else:
-        uniques_extns.extend([unique_extensions(node) for node in puzzle.extensions])
-
-    return uniques_extns
-
-    #2 - the best one i have currently
-    uniques_extns = deque()
-    uniques_extns.append(puzzle)
-
-    while not uniques_extns.is_empty():
-        check_node = uniques_extns.remove()
-        if check_node not in uniques_extns:
-            uniques_extns.append(check_node)
-        if check_node.extensions():
-            uniques_extns.extend(check_node.extensions())
-
-    return uniques_extns
-
-    #3 - using dictionary?? they are unordered, only the keys are unique, but keys cannot be mutable... so ???
-    extns = {}
-    extns[1] = puzzle
-
-    if puzzle.extensions():
-        for grid in puzzle.extensions():
-            extns[]
-
-def is_empty(nodes):
-    """
-    Return whether nodes is empty.
-    Modified from lecture sample.
-
-    @type nodes: deque[PuzzleNode]
-    @rtype: Bool
-    """
-
-    return len(nodes) == 0
 
 # Class PuzzleNode helps build trees of PuzzleNodes that have
 # an arbitrary number of children, and a parent.
